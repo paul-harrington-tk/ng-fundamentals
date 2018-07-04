a7d6ff3369654514de538ac342950f86b365f061
Paul Harrington <paulh@ovationsgroup.com>
2018-07-04 12:03:51 +0200

    custom validators

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, Validators, FormGroup } from "@angular/forms";
+import { ISession } from "../event.model";
 
 @Component({
-    templateUrl: './create-session.component.html'
+    templateUrl: './create-session.component.html',
+    styles: [`
+        em { float:right; color: #E05C65; padding-left: 10px;}
+        .error input, .error select, .error textarea { background-color: #E3C3C5; }
+        .error ::-webkit-input-placeholder { color: #999; }
+        .error ::-moz-placeholder { color: #999; }
+        .error :-moz-placeholder { color: #999; }
+        .error :ms-input-placeholder { color: #999; }
+    `]
 })
 export class CreateSessionComponent implements OnInit {
-    private name: FormControl;
-    private presenter: FormControl;
-    private duration: FormControl;
-    private level: FormControl;
-    private abstract: FormControl;
+    name: FormControl;
+    presenter: FormControl;
+    duration: FormControl;
+    level: FormControl;
+    abstract: FormControl;
     newSessionForm: FormGroup;
     ngOnInit() {
         this.name = new FormControl('', Validators.required);
         this.presenter = new FormControl('', Validators.required);
         this.duration = new FormControl('', Validators.required);
         this.level = new FormControl('', Validators.required);
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), this.restrictedWords]);
         this.newSessionForm = new FormGroup({
             name: this.name,
             presenter: this.presenter,
@@ -25,8 +34,25 @@ export class CreateSessionComponent implements OnInit {
             abstract: this.abstract
         });
     }
-
+    presenterInvalid(): boolean {
+        return this.presenter.invalid && (this.presenter.dirty || this.presenter.touched)
+    }
+    nameInvalid() : boolean {
+        return this.name.invalid && (this.name.dirty || this.name.touched)
+    }
+    private restrictedWords(control: FormControl): {[key:string]: any} {
+        return control.value.includes('foo') ? {'restrictedWords': 'foo'} : null;
+    }
     saveSession(formValues) {
-        console.log(formValues);
+        let session:ISession = {
+            id: undefined,
+            name: formValues.name,
+            presenter: formValues.presenter,
+            duration: +formValues.duration,
+            level: formValues.level,
+            abstract: formValues.abstract,
+            voters: []
+        }
+        console.log(session);
     }
 }
