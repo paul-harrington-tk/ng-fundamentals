90fd22311de5901a111c32a61aeaeb7ce06d5559
Paul Harrington <paulh@ovationsgroup.com>
2018-07-05 10:12:23 +0200

    profile saved toastr

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
+import { TOASTR_TOKEN, Toastr } from '../common/toastr.service';
 
 @Component({
     templateUrl: './profile.component.html',
@@ -16,7 +17,9 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
     profileForm: FormGroup;
-    constructor(private authService: AuthService, private router: Router, ) {
+    constructor(private authService: AuthService, 
+        private router: Router, 
+        @Inject(TOASTR_TOKEN) private toastr: Toastr) {
 
     }
     ngOnInit() {
@@ -42,6 +45,7 @@ export class ProfileComponent implements OnInit {
     saveProfile(formValues) {
         if (this.profileForm.valid) {
             this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+            this.toastr.success("Profile Updated");
         }
     }
     cancel() {
